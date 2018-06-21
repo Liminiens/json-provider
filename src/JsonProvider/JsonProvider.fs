88a52ae85b1947e9edaf165bc430d1f500cd244e
Vladislav Khapin <hapinvlad@gmail.com>
2018-06-21 22:44:19 +0300

    fixed sources, removed commented example code

--- src/JsonProvider/JsonProvider.fs
+++ src/JsonProvider/JsonProvider.fs
@@ -46,51 +46,5 @@ type JsonProvider (config : TypeProviderConfig) as this =
     do
        this.AddNamespace(ns, [generatedType; sampleType])
 
-(*
-type SomeRuntimeHelper() = 
-    static member Help() = "help"
-
-[<AllowNullLiteral>]
-type SomeRuntimeHelper2() = 
-    static member Help() = "help"
-
-[<TypeProvider>]
-type public ComboGenerativeProvider (config : TypeProviderConfig) as this =
-    inherit TypeProviderForNamespaces (config)
-
-    let ns = "ComboProvider"
-    let asm = Assembly.GetExecutingAssembly()
-
-    // check we contain a copy of runtime files, and are not referencing the runtime DLL
-    do assert (typeof<SomeRuntimeHelper>.Assembly.GetName().Name = asm.GetName().Name)  
-
-    let createType typeName (count:int) =
-        let asm = ProvidedAssembly()
-        let myType = ProvidedTypeDefinition(asm, ns, typeName, Some typeof<obj>, isErased=false)
-
-        let ctor = ProvidedConstructor([], invokeCode = fun args -> <@@ "My internal state" :> obj @@>)
-        myType.AddMember(ctor)
-
-        let ctor2 = ProvidedConstructor([ProvidedParameter("InnerState", typeof<string>)], invokeCode = fun args -> <@@ (%%(args.[1]):string) :> obj @@>)
-        myType.AddMember(ctor2)
-
-        for i in 1 .. count do 
-            let prop = ProvidedProperty("Property" + string i, typeof<int>, getterCode = fun args -> <@@ i @@>)
-            myType.AddMember(prop)
-
-        let meth = ProvidedMethod("StaticMethod", [], typeof<SomeRuntimeHelper>, isStatic=true, invokeCode = (fun args -> Expr.Value(null, typeof<SomeRuntimeHelper>)))
-        myType.AddMember(meth)
-        asm.AddTypes [ myType ]
-
-        myType
-
-    let myParamType = 
-        let t = ProvidedTypeDefinition(asm, ns, "GenerativeProvider", Some typeof<obj>, isErased=false)
-        t.DefineStaticParameters( [ProvidedStaticParameter("Count", typeof<int>)], fun typeName args -> createType typeName (unbox<int> args.[0]))
-        t
-    do
-        this.AddNamespace(ns, [myParamType])
-*)
-
 [<assembly:CompilerServices.TypeProviderAssembly()>]
 do ()
