849446a6f5af63f4a53b0e4e178a87e4dba5902e
Vladislav Khapin <hapinvlad@gmail.com>
2018-09-28 12:10:02 +0300

    Remove erased provider

--- tests/JsonProvider.Tests/JsonProvider.Tests.fs
+++ tests/JsonProvider.Tests/JsonProvider.Tests.fs
@@ -4,19 +4,6 @@ module JsonProviderTests
 open MyNamespace
 open NUnit.Framework
 
-[<Test>]
-let ``Default constructor should create instance`` () =
-    Assert.AreEqual("My internal state", MyType().InnerState)
-
-[<Test>]
-let ``Constructor with parameter should create instance`` () =
-    Assert.AreEqual("override", MyType("override").InnerState)
-
-[<Test>]
-let ``Method with ReflectedDefinition parameter should get its name`` () =
-    let myValue = 2
-    Assert.AreEqual("myValue", MyType.NameOf(myValue))
-
 type Generative2 = JsonProvider.GenerativeProvider<2>
 type Generative4 = JsonProvider.GenerativeProvider<4>
 
