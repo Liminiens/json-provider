fd34082b399c1eb40e690636630e3f4b4045f5f5
Vladislav Khapin <hapinvlad@gmail.com>
2018-10-30 10:30:50 +0300

    Add example

---
+++ examples/JsonProvider.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JsonProvider.Provider;
+using Newtonsoft.Json.Linq;
+
+namespace JsonProvider.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = ProviderExampleType.Value.Data;
+            foreach (var value in data)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ examples/JsonProvider.Provider/Library.fs
@@ -0,0 +1,5 @@
+namespace JsonProvider.Provider
+
+open FSharp.Liminiens.JsonProvider
+
+type ProviderExampleType = JsonProvider<"""{ "Data": [1,2,3] }""">
