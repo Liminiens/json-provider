3dea6896703dff9bf0ae41b22b7147709aaf7ecb
Vladislav Khapin <hapinvlad@gmail.com>
2018-10-30 10:31:18 +0300

    Fix tests

--- tests/JsonProvider.Tests/JsonProvider.Tests.fs
+++ tests/JsonProvider.Tests/JsonProvider.Tests.fs
@@ -2,7 +2,7 @@ module JsonProviderTests
 
 open FSharp.Liminiens.JsonProvider
 open NUnit.Framework
-
+open Newtonsoft.Json.Linq
 
 module Tests = 
 
@@ -11,20 +11,29 @@ module Tests =
     [<Test>]
     let ``Array int test`` () =
         let expected = [1; 2; 3]
-        CollectionAssert.AreEquivalent(expected, ArrayIntType.Value.Data)
+        let data = ArrayIntType.Value.Data
+
+        Assert.AreEqual(typeof<int32[]>, data.GetType())
+        CollectionAssert.AreEquivalent(expected, data)
         
     type ArrayDecimalType = JsonProvider<"""{ "Data": [1.2, 2, 3] }""">
 
     [<Test>]
     let ``Array decimal test`` () =
         let expected = [1.2M; 2M; 3M]
-        CollectionAssert.AreEquivalent(expected, ArrayDecimalType.Value.Data)
+        let data = ArrayDecimalType.Value.Data
+
+        Assert.AreEqual(typeof<decimal[]>, data.GetType())
+        CollectionAssert.AreEquivalent(expected, data)
 
     type ArrayLongType = JsonProvider<"""{ "Data": [100000000000, 2, 3] }""">
 
     [<Test>]
     let ``Array long test`` () =
         let expected = [100000000000L; 2L; 3L]
+        let data = ArrayLongType.Value.Data
+
+        Assert.AreEqual(typeof<int64[]>, data.GetType())
         CollectionAssert.AreEquivalent(expected, ArrayLongType.Value.Data)
         
     type ArrayObjectType = JsonProvider<"""{ "Data": [{"Test": 1}, {"Test": 2}] }""">
@@ -32,6 +41,7 @@ module Tests =
     [<Test>]
     let ``Array simple object test`` () =
         let data = ArrayObjectType.Value.Data
+
         Assert.AreEqual(1, data.[0].Test)
         Assert.AreEqual(2, data.[1].Test)
 
@@ -41,5 +51,6 @@ module Tests =
     [<Test>]
     let ``Array mixed test`` () =
         let data = ArrayMixedType.Value.Data
-        Assert.AreEqual(1, data.SelectToken("[*].Test").Value<int32>())
+
+        Assert.AreEqual(1, data.SelectToken("[0].Test").Value<int32>())
         Assert.AreEqual(2, data.[1].Value<int>())
